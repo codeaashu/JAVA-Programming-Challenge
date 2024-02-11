c1307eec438246a8f7f033f2c8d7a1880859b020
Ashutosh Singh <130897584+codeaashu@users.noreply.github.com>
2024-02-12 00:45:55 +0530

    🍵

---
+++ challenge2.java
@@ -0,0 +1,14 @@
+// show the following patterns using single print statement.🚀
+// Right Half Pyramid
+// Reverse Right Half Pyramid
+// Left Half Pyramid
+public class challenge2 {
+    public static void main(String[] args) {
+        // Right Half Pyramid
+        System.out.println("*\n*\t*\n*\t*\t*\n*\t*\t*\t*\n*\t*\t*\t*\t*\n\n");
+        // Reverse Right Half Pyramid
+        System.out.println("*\t*\t*\t*\t*\n*\t*\t*\t*\n*\t*\t*\n*\t*\n*\n\n");
+        // Left Half Pyramid
+        System.out.println("\t\t\t\t*\n\t\t\t*\t*\n\t\t*\t*\t*\n\t*\t*\t*\t*\n*\t*\t*\t*\t*");
+    }
+}
